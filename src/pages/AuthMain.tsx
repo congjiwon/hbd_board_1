6b350a955d386f9808a2ec4d054e92cc720d2f93
congjiwon <sheepg1510@gmail.com>
2023-08-04 00:29:26 +0900

    feat/후발대과제

--- src/pages/AuthMain.tsx
+++ src/pages/AuthMain.tsx
@@ -11,13 +11,13 @@ const AuthMain: React.FC = () => {
 
   const handleGoToLogin = () => {
     alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
-
+    setIsLogin(true);
     // TODO: 선택 시, LoginForm이 보이도록 제어
   };
 
   const handleGoToSignup = () => {
     alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
-
+    setIsLogin(false);
     // TODO: 선택 시, SignupForm이 보이도록 제어
   };
 
@@ -32,7 +32,7 @@ const AuthMain: React.FC = () => {
       >
         <div>
           <Logo>HBD</Logo>
-          {isLogin ? <LoginForm /> : <SignupForm />}
+          {isLogin ? <LoginForm /> : <SignupForm setIsLogin={setIsLogin} />}
           <Button
             type="link"
             onClick={isLogin ? handleGoToSignup : handleGoToLogin}
